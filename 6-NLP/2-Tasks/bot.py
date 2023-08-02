bfc58af8fcfea12714f60c135d96dd0b518b3a19
RoterHerzog <82552351+RoterHerzog@users.noreply.github.com>
2023-08-02 15:53:45 +0300

    Update for assignment

--- 6-NLP/2-Tasks/bot.py
+++ 6-NLP/2-Tasks/bot.py
@@ -10,32 +10,42 @@ def main():
     print("How are you today?")
 
     while True:
-        # wait for the user to enter some text
+ 
         user_input = input("> ")
 
         if user_input.lower() == "bye":            
-            # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
+           
             break
         else:
-            # Create a TextBlob based on the user input. Then extract the noun phrases
+            
             user_input_blob = TextBlob(user_input, np_extractor=extractor)                        
             np = user_input_blob.noun_phrases                                    
             response = ""
-            if user_input_blob.polarity <= -0.5:
-                response = "Oh dear, that sounds bad. "
-            elif user_input_blob.polarity <= 0:
-                response = "Hmm, that's not great. "
-            elif user_input_blob.polarity <= 0.5:
-                response = "Well, that sounds positive. "
-            elif user_input_blob.polarity <= 1:
-                response = "Wow, that sounds great. "
 
+            if user_input_blob.polarity <= -0.5:
+                response = "Negative Emotion Detected: Sadness "
+
+            elif user_input_blob.polarity <= 0:
+                response = "Negative Emotion Detected: Neutrality "
+
+            elif user_input_blob.polarity <= 0.5:
+                response = "Negative Emotion Detected: Pozitivity "
+
+            elif user_input_blob.polarity <= 1:
+                response = "Negative Emotion Detected: Pozitivity "
+
+            if response == "Negative Emotion Detected: Sadness ":
+                response = "Negativity is bad for your mental health. Sometimes telling your feelings to others can be helpfull. Do you consider to telling me more about that stuff"
+                print(response)
+                response2 = input(">")
+                print("I really cannot understand due to the fact I'm just a program")
+                
             if len(np) != 0:
-                # There was at least one noun phrase detected, so ask about that and pluralise it
-                # e.g. cat -> cats or mouse -> mice
                 response = response + "Can you tell me more about " + np[0].pluralize() + "?"
+
             else:
                 response = response + "Can you tell me more?"
+
             print(response)
     
     print("It was nice talking to you, goodbye!")
